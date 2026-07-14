39721304eef726e8f1f1612f63b611d0e56351c2
Irdina <irdina.binti.ismail@hs-augsburg.de>
2026-07-14 21:07:47 +0200

    modify db .js script

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,24 @@
 import mongoose from "mongoose";
 
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MONGODB CONNECTED");
+//   } catch (error) {
+//     console.error("error connecting to MONGODB", error);
+//     process.exit(1);
+//   }
+// };
+
 export const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is missing");
+    }
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MONGODB CONNECTED");
   } catch (error) {
-    console.error("error connecting to MONGODB", error);
-    process.exit(1);
+    console.error("MONGODB ERROR:", error);
   }
 };
